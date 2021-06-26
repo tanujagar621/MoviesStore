6232123a2b09531f1e581b98784dbae3dfb49862
tanujagar621 <tanujagarwal621@gmail.com>
2021-06-26 13:49:35 +0530

    fixed popular showa

--- routes/popular.js
+++ routes/popular.js
@@ -78,31 +78,4 @@ router.get("/mostpopular/:type", async (req, res) => {
     }
 });
 
-router.get("/mostpopular/tv", async (req, res) => {
-  const url_api =
-    `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`;
-
-    try {
-      await fetch(url_api)
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.total_results === 0) {
-            res.render("popular", {
-              error: "Nothing Found",
-              results: null,
-              type: "tv",
-            });
-          } else {
-            res.render("popular", {
-              error: null,
-              results: data.results,
-              type: "tv",
-            });
-          }
-        });
-    } catch (err) {
-      console.log(err);
-    }
-});
-
 module.exports = router;
